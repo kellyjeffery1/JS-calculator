fe2cbef5aea225fca692109a22bbad3abdb7d90f
kellyjeffery <kellyjeffery1@hotmail.com>
2020-01-29 18:36:36 +0000

    First commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function calculate() {
+  const a = parseInt(document.querrySelector('#value1').value);
+  const b = parseInt(document.querrySelector('#value2').value);
+  const op = document.querrySelector('#operator').value;
+  var calculate;
+
+  if (op == "add"){
+    calculate = a + b;
+  } else if (op == "min") {
+    calculate = a - b;
+  } else if (op == "div") {
+    calculate = a / b;
+  } else if (op == "mul") {
+    calculate = a * b;
+ }
+ document.querrySelector('#result').innerHTML = calculate;
+}
